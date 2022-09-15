496546db952e4927a436662976877e04e7c2f6ec
Nosedive22 <112674472+Nosedive22@users.noreply.github.com>
2022-09-16 00:23:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ogSnL6d-icTR7sOa25I5yAeeEyMw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "geyOlWk7Fjz4-U8T3KtISam83IlkOPx_4TpZ6SF_oqU",
+      useTemplateId: "CAYXES6ZqkaQ9RaY6ltJ8cfkiGStWBfBlvZJ9w2Nq7g",
       // 所在省份
       province: "河南",
       // 所在城市
@@ -79,12 +79,11 @@ export const config = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {"type": "*生日", "name": "你", "year": "2000", "date": "11-14"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        // {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+         // {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+         // {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
       ],
-      festivals_LIMIT: 1,
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
